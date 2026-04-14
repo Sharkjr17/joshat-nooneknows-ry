9c008b9fdaa2d98a0e87a327a2074af98bd06cf2
Bananoker <169085955+Bananoker@users.noreply.github.com>
2026-04-14 15:19:05 +0000

    message: Thanks Josh

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+// 1. Select the button using its ID
+const startButton = document.getElementById("start-button");
 
+// 2. Add a 'click' event listener
+startButton.addEventListener("click", function() {
+    alert("Button was clicked!");
+    startButton.disabled = true;
+});
